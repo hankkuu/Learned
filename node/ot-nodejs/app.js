85508a38b83690533f3ce676c1832d20bc7c7bca
Jinho Hyeon <neverlish@gmail.com>
2017-02-07 22:17:40 +0900

    [node] ot-nodejs : 간단한 웹어플리케이션 제작

--- node/ot-nodejs/app.js
+++ node/ot-nodejs/app.js
@@ -1,79 +1,58 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var fs = require('fs');
 
 app.locals.pretty = true;
 
 app.set('view engine', 'jade');
 app.set('views', './views');
-app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: false}));
 
-/// 정적 페이지 렌더링
-
-app.get('/', function(req, res) {
-  res.send('Hello homepage');
+/// 글 작성
+app.get('/topic/new', function(req, res) {
+  fs.readdir('data', function(err, files) {
+    if (err) {
+      console.log(err);
+      res.status(500).send('Internal Server Error');
+    }
+    res.render('new', {topics:files});
+  });
 });
 
-app.get('/login', function(req, res) {
-  res.send('Login please');
-});
-
-app.get('/template', function(req, res) {
-  res.render('temp', {time: Date(), title: 'Jade'});
-});
-
-/// 동적 페이지 렌더링
-
-app.get('/dynamic', function(req, res) {
-  var lis = '';
-  for (var i=0; i<5; i++) {
-    lis = lis + '<li>coding</li>'
-  };
-  var time = Date();
-  var output = `
-    <!DOCTYPE html>
-    <html>
-      <head>
-        <meta charset='utf-8'>
-        <title></title>
-      </head>
-      <body>
-        Hello, Dynamic
-        <ul>
-          ${lis}
-        </ul>
-        ${time}
-      </body>
-    </html>`;
-  res.send(output);
-});
-
-app.get('/topic/:id', function(req, res) {
-  var topics = [
-    'Javascript is ...',
-    'Nodejs is ...',
-    'Express is ...'
-  ];
-  var as = `
-    <a href='/topic/0'>Javascript</a><br>
-    <a href='/topic/1'>Nodejs</a><br>
-    <a href='/topic/2'>Expres</a><br><br>
-    ${topics[req.params.id]}`;
-  res.send(as);
-});
-
-/// POST 방식을 통한 정보 전달
-
-app.get('/form', function(req, res) {
-  res.render('form');
-});
-
-app.post('/form_receiver', function(req, res) {
+app.post('/topic', function(req, res) {
   var title = req.body.title;
   var description = req.body.description;
-  res.send(title + ', ' + description);
-  res.send('Hello, POST');
+  fs.writeFile('data/'+title, description, function(err) {
+    if (err) {
+      console.log(err);
+      res.status(500).send('Internal Server Error');
+    }
+    res.redirect('/topic/'+title);
+  });
+});
+
+/// 글 목록 및 상세 조회
+app.get(['/topic', '/topic/:id'], function(req, res) {
+  fs.readdir('data', function(err, files) {
+    if (err) {
+      console.log(err);
+      res.status(500).send('Internal Server Error');
+    }
+    var id = req.params.id;
+    if (id) {
+      // id 값이 있을 때
+      fs.readFile('data/'+id, 'utf8', function(err, data) {
+        if (err) {
+          console.log(err);
+          res.status(500).send('Internal Server Error');
+        }
+        res.render('view', {topics: files, title: id, description: data});
+      })
+    } else {
+      res.render('view', {topics: files, title: 'Welcome', description: 'Hello, Javascript for server.'});
+    }
+  });
 });
 
 app.listen(3000, function() {
