816f1267e6b71ef81e5e0323985128e2dcbcd7fa
Jinho Hyeon <neverlish@gmail.com>
2017-02-07 22:39:46 +0900

    [node] ot-nodejs : cookie - shopping cart

--- node/ot-nodejs/app.js
+++ node/ot-nodejs/app.js
@@ -35,6 +35,56 @@ app.get('/count', function(req, res) {
   res.send('count: ' + count);
 });
 
+var products = {
+  1: {title: 'The history of web 1'},
+  2: {title: 'The next web'}
+};
+
+app.get('/products', function(req, res) {
+  var output = '';
+  for (var name in products) {
+    output += `
+      <li>
+        <a href='/cart/${name}'>${products[name].title}</a>
+      </li>`;
+  };
+  res.send(`<h1>Products</h1><ul>${output}</ul><a href='/cart'>Cart</a>`)
+})
+
+/// 카트 기능 구현
+app.get('/cart/:id', function(req, res) {
+  var id = req.params.id;
+  if (req.cookies.cart) {
+    var cart = req.cookies.cart;
+  } else {
+    var cart = {};
+  }
+
+  if (!cart[id]) {
+    cart[id] = 0;
+  }
+  cart[id] = parseInt(cart[id]) + 1;
+  res.cookie('cart', cart);
+  res.redirect('/cart');
+});
+
+app.get('/cart', function(req, res) {
+  var cart = req.cookies.cart;
+  if (!cart) {
+    res.send('Empty');
+  } else {
+    var output = '';
+    for (var id in cart) {
+      output += `<li>${products[id].title} (${cart[id]})</li>`
+    }
+  }
+  res.send(`
+    <h1>Cart</h1>
+    <ul>${output}</ul>
+    <a href='/products'>Products List</a>
+  `);
+})
+
 /// 글 작성
 app.get('/topic/new', function(req, res) {
   fs.readdir('data', function(err, files) {
