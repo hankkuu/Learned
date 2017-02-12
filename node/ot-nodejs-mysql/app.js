95e65a73176799e4635dc2b31f8815d6e155bc44
Jinho Hyeon <jinhohyeon@Jinhoui-MacBook-Pro.local>
2017-02-12 17:06:26 +0900

    [node] ot-nodejs-mysql : CRUD - Delete

--- node/ot-nodejs-mysql/app.js
+++ node/ot-nodejs-mysql/app.js
@@ -100,6 +100,31 @@ app.post('/topic/:id/edit', function(req, res) {
   });
 });
 
+// 글 삭제
+app.get('/topic/:id/delete', function(req, res) {
+  var sql = 'SELECT id, title FROM topic';
+  conn.query(sql, function(err, topics, fields) {
+    var sql = 'SELECT * FROM topic WHERE id=?';
+    var id = req.params.id;
+    conn.query(sql, [id], function(err, topic, fields) {
+      if (err) {
+        console.log(err);
+        res.status(500).send('Internal Server Error');
+      } else {
+        res.render('delete', {topics: topics, topic: topic[0]});
+      }
+    });
+  });
+});
+
+app.post('/topic/:id/delete', function(req, res) {
+  var id = req.params.id;
+  var sql = 'DELETE FROM topic WHERE id=?'
+  conn.query(sql, [id], function(err, result) {
+    res.redirect('/topic');
+  })
+})
+
 app.listen(3000, function() {
   console.log('Connected, 3000 port!');
 })
