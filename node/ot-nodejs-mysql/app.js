0cbd120562c2d7d3d0a4cccd22032c76d97cc8bd
Jinho Hyeon <jinhohyeon@Jinhoui-MacBook-Pro.local>
2017-02-12 16:52:36 +0900

    [node] ot-nodejs-mysql : CRUD - Create

--- node/ot-nodejs-mysql/app.js
+++ node/ot-nodejs-mysql/app.js
@@ -14,6 +14,34 @@ app.set('views', './views');
 app.set('view engine', 'jade');
 app.use(bodyParser.urlencoded({extended: false}));
 
+/// 글 작성
+app.get('/topic/new', function(req, res) {
+  var sql = 'SELECT id, title FROM topic';
+  conn.query(sql, function(err, topics, fields) {
+    if (err) {
+      console.log(err);
+      res.status(500).send('Internal Server Error');
+    } else {
+      res.render('new', {topics: topics});
+    }
+  });
+});
+
+app.post('/topic/new', function(req, res) {
+  var title = req.body.title;
+  var description = req.body.description;
+  var author = req.body.author;
+  var sql = 'INSERT INTO topic (title, description, author) VALUES (?, ?, ?)';
+  conn.query(sql, [title, description, author], function(err, rows, fields) {
+    if (err) {
+      console.log(err);
+      res.status(500).send('Internal Server Error');
+    } else {
+      res.redirect('/topic/'+rows.insertId);
+    }
+  });
+});
+
 app.get(['/topic', '/topic/:id'], function(req, res) {
   var sql = 'SELECT id, title FROM topic';
   conn.query(sql, function(err, topics, fields) {
