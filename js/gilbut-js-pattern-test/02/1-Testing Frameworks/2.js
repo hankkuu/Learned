68e05cad34fd2d0f52da6d0ede20f640a5588c72
Jinho Hyeon <neverlish@gmail.com>
2018-06-23 07:55:08 +0900

    02 도구 다루기 - 1 테스팅 프레임워크

---
+++ js/gilbut-js-pattern-test/02/1-Testing Frameworks/2.js
@@ -0,0 +1,8 @@
+// 02-1-1 잘못된 코드 발견하기 - TDD로 구현한 createReservation 초기 코드
+
+function createReservation(passenger, flight) {
+  return {
+    passengerInfomation: passenger,
+    flightInfomation: flight
+  };
+}
---
+++ js/gilbut-js-pattern-test/02/1-Testing Frameworks/3.js
@@ -0,0 +1,8 @@
+// 02-1-7 재스민 들어가기 - 테스트 꾸러미와 스펙 - createReservation 완성본
+
+function createReservation(passenger, flight) {
+  return {
+    passengerInformation: passenger,
+    flightInformation: flight
+  };
+}
